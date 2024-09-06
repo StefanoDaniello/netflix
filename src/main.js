b66e4738984dad46fe65ea9294e5f700f5602b0e
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-09-06 16:56:52 +0200

    add comment

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './assets/style/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+//controllo se il browser dell'utente supporta il service worker
 if('serviceWorker' in navigator){
+    //attendo che tutti gli assets siano completamente caricati
     window.addEventListener('load', function () {
+        //registro il file sw nel broswer
         navigator.serviceWorker
         .register('/service-worker.js')
         .then(function () {
